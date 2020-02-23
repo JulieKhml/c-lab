6950558ef3a6957725a060ff77e9badffcdea798
JulieKhml <julie.khmil@gmail.com>
2020-02-23 12:05:26 +0200

    add files from first lab

---
+++ Khmil_lab_1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Khmil_lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.year_old = new System.Windows.Forms.TextBox();
+            this.chinese_zodiac_signs = new System.Windows.Forms.TextBox();
+            this.west_zodiac_signs = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(92, 77);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(186, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Виберыть день вашого народження";
+            // 
+            // year_old
+            // 
+            this.year_old.Location = new System.Drawing.Point(177, 113);
+            this.year_old.Name = "year_old";
+            this.year_old.ReadOnly = true;
+            this.year_old.Size = new System.Drawing.Size(101, 20);
+            this.year_old.TabIndex = 3;
+            // 
+            // chinese_zodiac_signs
+            // 
+            this.chinese_zodiac_signs.Location = new System.Drawing.Point(177, 150);
+            this.chinese_zodiac_signs.Name = "chinese_zodiac_signs";
+            this.chinese_zodiac_signs.ReadOnly = true;
+            this.chinese_zodiac_signs.Size = new System.Drawing.Size(101, 20);
+            this.chinese_zodiac_signs.TabIndex = 4;
+            // 
+            // west_zodiac_signs
+            // 
+            this.west_zodiac_signs.Location = new System.Drawing.Point(177, 193);
+            this.west_zodiac_signs.Name = "west_zodiac_signs";
+            this.west_zodiac_signs.ReadOnly = true;
+            this.west_zodiac_signs.Size = new System.Drawing.Size(101, 20);
+            this.west_zodiac_signs.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Вік";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Китайский ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Західний";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 342);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.west_zodiac_signs);
+            this.Controls.Add(this.chinese_zodiac_signs);
+            this.Controls.Add(this.year_old);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox year_old;
+        private System.Windows.Forms.TextBox chinese_zodiac_signs;
+        private System.Windows.Forms.TextBox west_zodiac_signs;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Khmil_lab_1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// http://horo.tochka.net/ua/horoscopes/sign/
+// https://www.chinahighlights.ru/kakoy-god-kakogo-zhivotnogo/
+
+namespace Khmil_lab_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            int myAge = DateTime.Today.Year - dateTimePicker1.Value.Year;
+            year_old.Text = myAge.ToString();
+            if (myAge < 0 || myAge > 135)
+            { // CurrentYear - YourBirthDate
+                year_old.Text = "Не можливо обрахувати";
+                MessageBox.Show("Не вірна вибрана дата народження або менше 0 років або більше 135 років.");
+            }
+
+            if (dateTimePicker1.Value.Day == DateTime.Today.Day && dateTimePicker1.Value.Month == DateTime.Today.Month)
+            {
+                MessageBox.Show("З днем народження))). Це приємне повідомлення.");
+            }
+
+            chinese_zodiac_signs.Text = ChineseZodiac(dateTimePicker1.Value);
+            west_zodiac_signs.Text = WestZodiac(dateTimePicker1.Value);
+        }
+        private string ChineseZodiac(DateTime date)
+        {
+            string[] zodiacSigns = {
+                "Щур",
+                "Бик",
+                "Тигр",
+                "Кролик",
+                "Дракон",
+                "Змія",
+                "Кінь",
+                "Вівці",
+                "Мавпа",
+                "Півень",
+                "Собака",
+                "Свиня"
+            };
+
+            const int ZodiacStartYear = 4; //1960
+            const int Months = 12;
+
+            int remainder = (date.Year - ZodiacStartYear) % Months;
+
+            return zodiacSigns[remainder];
+        }
+
+        private string WestZodiac(DateTime date)
+        {
+            switch (date.Month)
+            {
+                case 1:
+                    if (date.Day <= 20)
+                    { return "Козеріг"; }
+                    else
+                    { return "Водолій"; }
+                case 2:
+                    if (date.Day <= 18)
+                    { return "Водолій"; }
+                    else
+                    { return "Риби"; }
+                case 3:
+                    if (date.Day <= 20)
+                    { return "Риби"; }
+                    else
+                    { return "Овен"; }
+                case 4:
+                    if (date.Day <= 20)
+                    { return "Овен"; }
+                    else
+                    { return "Тельці"; }
+                case 5:
+                    if (date.Day <= 21)
+                    { return "Тельці"; }
+                    else
+                    { return "Близнюки"; }
+                case 6:
+                    if (date.Day <= 21)
+                    { return "Близнюки"; }
+                    else
+                    { return "Рак"; }
+                case 7:
+                    if (date.Day <= 22)
+                    { return "Рак"; }
+                    else
+                    { return "Лев"; }
+                case 8:
+                    if (date.Day <= 23)
+                    { return "Лев"; }
+                    else
+                    { return "Діва"; }
+                case 9:
+                    if (date.Day <= 23)
+                    { return "Діва"; }
+                    else
+                    { return "Терези"; }
+                case 10:
+                    if (date.Day <= 23)
+                    { return "Терези"; }
+                    else
+                    { return "Скорпіон"; }
+                case 11:
+                    if (date.Day <= 22)
+                    { return "Скорпіон"; }
+                    else
+                    { return "Стрілець"; }
+                case 12:
+                    if (date.Day <= 21)
+                    { return "Стрілець"; }
+                    else
+                    { return "Козеріг"; }
+                default:
+                    return "Ваш знак зодіаку не знайшли! Спробуйте ще раз!";
+            }
+        }
+    }
+}
